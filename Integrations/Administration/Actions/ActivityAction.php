0413ceac44f75649642f40558d3413f6c482e32a
COLANT Alan <alan.colant@uni-deal.com>
2025-12-12 16:51:10 +0100

    chore: rework local module registering with bootstrap/modules.php

---
+++ Integrations/Administration/Actions/ActivityAction.php
@@ -0,0 +1,96 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EpsicubeModules\ExecutionPlatform\Integrations\Administration\Actions;
+
+use Closure;
+use EpsicubeModules\ExecutionPlatform\Facades\Activities;
+use Filament\Actions\Action;
+use Filament\Support\Enums\Operation;
+use Throwable;
+
+class ActivityAction extends Action
+{
+    protected Closure|string|null $activityIdentifier = null;
+
+    /**
+     * @var array<string, mixed>
+     */
+    protected Closure|array $activityConstants = [];
+
+    protected ?array $activityResult = null;
+
+    protected ?Throwable $activityError = null;
+
+    public function activity(string|Closure $identifier, array|Closure $constants = []): static
+    {
+        $this->activityIdentifier = $identifier;
+        $this->activityConstants = $constants;
+
+        return $this;
+    }
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->schema(function (self $action): array {
+            $schema = Activities::inputSchema($action->getActivityIdentifier());
+            $constants = $this->getActivityConstants();
+            if (! empty($constants)) {
+                $schema = $schema->except(...array_keys($constants));
+            }
+
+            return $schema->toFilamentComponents(Operation::Create);
+        });
+
+        $this->successNotificationTitle(__('Activity successfully executed'));
+        $this->failureNotificationTitle(__('Error executing activity'));
+        $this->failureNotificationBody(
+            fn (?Throwable $error) => app()->hasDebugModeEnabled() ? $error?->getMessage() : null
+        );
+
+        $this->action(function (self $action, array $data) {
+            $data = array_merge($this->getActivityConstants(), $data);
+            try {
+                $action->activityResult = Activities::run($this->getActivityIdentifier(), $data);
+                $action->success();
+            } catch (Throwable  $e) {
+                report($this->activityError = $e);
+                $action->failure();
+            }
+        });
+
+        $this->modalSubmitActionLabel(__('Execute'));
+    }
+
+    protected function resolveDefaultClosureDependencyForEvaluationByName(string $parameterName): array
+    {
+        return match ($parameterName) {
+            'result' => [$this->getActivityResult()],
+            'error'  => [$this->getActivityError()],
+            default  => parent::resolveDefaultClosureDependencyForEvaluationByName($parameterName)
+        };
+    }
+
+    public function getActivityIdentifier(): string
+    {
+        return $this->evaluate($this->activityIdentifier);
+    }
+
+    public function getActivityConstants(): array
+    {
+        return $this->evaluate($this->activityConstants);
+    }
+
+    protected function getActivityResult(): array
+    {
+        return $this->activityResult;
+    }
+
+    protected function getActivityError(): Throwable
+    {
+        return $this->activityError;
+    }
+}
