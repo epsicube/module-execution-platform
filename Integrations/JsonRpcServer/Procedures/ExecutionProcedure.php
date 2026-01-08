d171268029a820f529e6841c58443ef01d112d42
COLANT Alan <alan.colant@uni-deal.com>
2026-01-08 15:47:11 +0100

    feat: add listActivities json-rpc method

--- Integrations/JsonRpcServer/Procedures/ExecutionProcedure.php
+++ Integrations/JsonRpcServer/Procedures/ExecutionProcedure.php
@@ -6,6 +6,7 @@ namespace EpsicubeModules\ExecutionPlatform\Integrations\JsonRpcServer\Procedure
 
 use Epsicube\Schemas\Properties\ObjectProperty;
 use Epsicube\Schemas\Schema;
+use EpsicubeModules\ExecutionPlatform\Contracts\Activity;
 use EpsicubeModules\ExecutionPlatform\Facades\Activities;
 use EpsicubeModules\JsonRpcServer\Concerns\Procedure;
 use Illuminate\Http\Request;
@@ -15,7 +16,20 @@ class ExecutionProcedure extends Procedure
 {
     public static string $name = 'execution';
 
-    public function activity(Request $request): array
+    public function listActivities(): array
+    {
+        $activities = Activities::all();
+
+        return collect($activities)->map(fn (Activity $activity, string $identifier) => [
+            'identifier'    => $identifier,
+            'name'          => $activity->label(),
+            'description'   => $activity->description(),
+            'input_schema'  => Activities::inputSchema($identifier)->toJsonSchema(),
+            'output_schema' => Activities::outputSchema($identifier)->toJsonSchema(),
+        ])->values()->all();
+    }
+
+    public function callActivity(Request $request): array
     {
         $validated = $request->validate([
             'identifier' => ['string', 'required', Rule::in(array_keys(Activities::all()))],
@@ -23,9 +37,9 @@ class ExecutionProcedure extends Procedure
 
         // Validating input schema
         $inputSchema = Schema::create('input', properties: [
-            'params' => ObjectProperty::make()->optional()->properties(Activities::inputSchema($validated['identifier'])->properties()),
+            'input' => ObjectProperty::make()->optional()->properties(Activities::inputSchema($validated['identifier'])->properties()),
         ]);
-        $validatedParams = data_get($inputSchema->validated($request->only('params')), 'params', []);
+        $validatedParams = data_get($inputSchema->validated($request->only('input')), 'input', []);
         $execution = Activities::run($validated['identifier'], $validatedParams);
 
         return $execution->only([
